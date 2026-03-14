14f018dd5b3caeb1941641b2900cce1e7a547f8f
root <root@ip-172-31-17-222.ec2.internal>
2026-03-14 10:18:33 +0000

    initial terraform-cicd demo

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo_server" {
+
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-CICD-Demo"
+  }
+
+}
